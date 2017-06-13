7ad4474d334eb98e1fc18cf9ae1666b46edfce5c
Robert Kaszubowski <robert_kaszubowski@epam.com>
2017-06-13 12:16:35 +0200

    header intergraton tests for sqrt

--- src/test/java/mathcalc/integration/SqrtTest.java
+++ src/test/java/mathcalc/integration/SqrtTest.java
@@ -1,14 +1,20 @@
 package mathcalc.integration;
 
+import com.jayway.restassured.response.Response;
 import helpers.ControllersUrlProvider;
+import org.apache.http.HttpStatus;
 import org.junit.Before;
 import org.junit.Test;
 
 import static com.jayway.restassured.RestAssured.baseURI;
 import static com.jayway.restassured.RestAssured.given;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 
 public class SqrtTest {
+
+    private static final String PROPER_VALUE = "64";
 
     @Before
     public void setUp() {
@@ -16,17 +22,35 @@ public class SqrtTest {
     }
 
     @Test
-    public void shouldReturn200WhenServiceIsUp() {
-        given().contentType("application/json").when().get("64").then().statusCode(200);
+    public void shouldReturnStatusOkWhenServiceIsUp() {
+        given()
+                .contentType("application/json")
+                .when().get(PROPER_VALUE)
+                .then().statusCode(HttpStatus.SC_OK);
     }
 
     @Test
-    public void shouldReturn404WhenEmptyParamIsPassed() {
-        given().contentType("application/json").when().get("").then().statusCode(404);
+    public void shouldReturnStatusNotFoundWhenEmptyParamIsPassed() {
+        given()
+                .contentType("application/json")
+                .when().get("")
+                .then().statusCode(HttpStatus.SC_NOT_FOUND);
+    }
+
+    @Test
+    public void shouldCheckIfHeaderContainsAllFields() {
+        given()
+                .header("Connection", "keep-alive")
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .header("Accept-Encoding", "gzip,deflate")
+                .when().log().method().get();
     }
 
     @Test
     public void shouldReturn8WhenParamEquals64() {
-        given().contentType("application/json").when().get("64").then().statusCode(200).body("output", containsString("8.0"));
+        Response response =  given().contentType("application/json").when().get("64");
+        String jsonString = response.asString();
+
+        assertThat(jsonString, containsString("8.0"));
     }
 }
